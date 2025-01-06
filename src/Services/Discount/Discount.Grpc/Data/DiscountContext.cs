32baae1ec94a641e28d06ebae6f169afa471ceb1
Dariusz Dudek <darek@ebud.pl>
2025-01-06 11:41:05 +0100

    Add discount ot docker

---
+++ src/Services/Discount/Discount.Grpc/Data/DiscountContext.cs
@@ -0,0 +1,17 @@
+using Discount.Grpc.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Discount.Grpc.Data;
+
+public class DiscountContext(DbContextOptions<DiscountContext> options) : DbContext(options)
+{
+    public DbSet<Coupon> Coupons { get; set; } = default!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Coupon>().HasData(
+            new Coupon {Id = 1, ProductName = "Iphone X", Description = "Iphone X discount description", Amount = 150},
+            new Coupon {Id = 2, ProductName = "Samsung 10", Description = "Samsung 10 discount description", Amount = 100}
+            );
+    }
+}
